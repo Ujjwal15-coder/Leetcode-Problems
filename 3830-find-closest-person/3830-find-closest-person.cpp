4701763c67e395256983f892de70a5f17d57e477
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-09-05 16:30:13 +0530

    Time: 0 ms (100%), Space: 8.7 MB (21.09%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.cpp
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -1,18 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    int whoReachesFirst(int x, int y, int z) {
-        int dist1 = abs(x - z);
-        int dist2 = abs(y - z);
-        if (dist1 < dist2) return 1;
-        else if (dist2 < dist1) return 2;
-        else return 0;
-    }
-
-    // wrapper with the expected name
     int findClosest(int x, int y, int z) {
-        return whoReachesFirst(x, y, z);
+     int a = abs(z - x);
+     int b = abs(z - y);
+
+     if(a == b) return 0;
+
+     if(a < b){
+        return 1;
+     }
+     return 2;
     }
 };
