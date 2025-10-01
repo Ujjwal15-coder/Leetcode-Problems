d7a984440b6882555e544255d7cdb6103fd4c968
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-10-01 15:44:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (45.09%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles;
+        int emptyBottles = numBottles;
+
+        while(emptyBottles >= numExchange){
+            int newBottles = emptyBottles / numExchange;
+            result += newBottles;
+            emptyBottles = newBottles + (emptyBottles % numExchange);
+
+        }
+
+        return result;
+    }
+};
