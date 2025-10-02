93b61589ae9edba08f23909f0af8dde720c96000
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-10-02 13:08:07 +0530

    Time: 5 ms (39.54%), Space: 64.7 MB (33.74%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.cpp
@@ -3,20 +3,24 @@ public:
     vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+
         int cnt_max = -1;
-        int index = -1;
+        int index = 0;
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n;i++){
+
             int cnt_ones = 0;
-            for (int j = 0; j < m; j++) {
-                cnt_ones += mat[i][j]; // sum of 1s
+
+            for(int j = 0; j < m; j++){
+                cnt_ones += mat[i][j];
             }
 
-            if (cnt_ones > cnt_max) {
-                cnt_max = cnt_ones;
-                index = i; // first row with max ones
-            }
+                if(cnt_ones > cnt_max){
+                    cnt_max = cnt_ones;
+                    index = i;
+                }
+            
         }
-        return {index, cnt_max};
+        return {index,cnt_max};
     }
 };
