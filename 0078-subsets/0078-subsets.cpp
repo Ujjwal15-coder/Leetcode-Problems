566ff79a5ac28437554d5beb87a7251401a7ffee
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-11-09 11:15:22 +0530

    Time: 0 ms (100%), Space: 10 MB (59.46%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+   void func(int ind,vector<int> &nums,vector<int> &ds,vector<vector<int>> &ans){
+    ans.push_back(ds);
+    for(int i = ind;i < nums.size();i++){
+        ds.push_back(nums[i]);
+        func(i + 1,nums,ds,ans);
+        ds.pop_back();
+    }
+   }
+
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        sort(nums.begin(),nums.end());
+        func(0,nums,ds,ans);
+        return ans;
+        
+    }
+};
