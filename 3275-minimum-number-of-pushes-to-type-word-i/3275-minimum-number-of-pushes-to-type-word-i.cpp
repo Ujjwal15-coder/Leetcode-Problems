081fac9cfda6cd739eba14bfb505fe7148e33a33
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-11-20 19:37:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (42.07%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        vector<int> mp(26,0);
+        
+        for(char &ch:word){
+            mp[ch -'a']++;
+        }
+        sort(begin(mp),end(mp),greater<int>());
         int result = 0;
 
-        unordered_map<int,int>mp;
-        int assign_key = 2;
-
-        for(char &ch: word){
-            if(assign_key > 9){
-                assign_key = 2;
-            }
-            mp[assign_key]++;
-            result += mp[assign_key];
-
-             assign_key++;
+        for(int i = 0;i < 26; i++){
+            int freq = mp[i];
+            int press = i/8 + 1;
+            result += press * freq;
         }
         return result;
     }
