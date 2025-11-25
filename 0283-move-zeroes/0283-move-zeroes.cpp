3c2a307cfd1bfff2d71ecdfe42d577958148d20c
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-11-25 23:30:41 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,15 +4,17 @@ public:
         int n = nums.size();
         int j = -1;
         for(int i = 0;i < n;i++){
-            if(nums[i]==0){
-                j = i;
-                break;
-            }
-    }
-    if(j == -1) return;
-
-        for(int i = j+1;i<n;i++){
-            if(nums[i]!=0){
+            if(nums[i] == 0)
+           {
+             j = i;
+           
+            break;
+           }
+        } 
+        if(j == -1) return;
+        
+        for(int i = j+1;i < n;i++){
+            if(nums[i] != 0){
                 swap(nums[i],nums[j]);
                 j++;
             }
