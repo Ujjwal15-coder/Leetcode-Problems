aeba621dbeec8f9ff5759c9c24290d3926218d05
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2025-12-14 00:06:07 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.53%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int XOR1=0,XOR2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            XOR2=XOR2^nums[i];
+            XOR1=XOR1^(i+1);
+            
+
+        }
+        return XOR1^XOR2;
+        
+    }
+};
