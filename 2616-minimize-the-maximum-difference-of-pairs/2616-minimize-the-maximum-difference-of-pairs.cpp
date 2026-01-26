7584ace71fa36328fbc75825492114d212de73c1
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-01-26 23:01:13 +0530

    Time: 23 ms (76.07%), Space: 86.7 MB (51.72%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,58 +1,41 @@
 class Solution {
 public:
-bool check(vector<int>&nums,int p,int mid){
-int k=0;
-    for(int i=0; i<nums.size()-1; i+=1){
-
+bool check(vector<int>& nums, int p,int mid){
+    int k = 0;
+    for(int i = 0; i < nums.size() - 1;i++){
         if(nums[i+1]-nums[i]<=mid){
-
             k++;
             i++;
-       if(k==p){
+
+        }
+        if(k==p) {
             return true;
         }
-
-        }
-     
-
-
     }
     return false;
-
-
-
-
-
 }
     int minimizeMax(vector<int>& nums, int p) {
         int n = nums.size();
-       sort(nums.begin(),nums.end());
-         int low=0;
-         int high=nums[n-1]-nums[0];
-         if(p==0){
+        sort(nums.begin(),nums.end());
+        int low = 0;
+        int high = nums[n-1] - nums[0];
+        int ans;
+        if(p == 0)  {
             return 0;
-         }
+        }
 
-int ans;
-         while(low<=high){
-
-
-            int mid=low+(high-low)/2;
-
-
+        while(low <= high)
+        {
+            
+            int mid = low + (high - low)/2;
             if(check(nums,p,mid)){
-
-                ans=mid;
-                high=mid-1;
-
-
+                ans = mid;
+                high = mid - 1;
             }
             else{
-                low=mid+1;
+                low = mid + 1;
             }
-         }
-
-
+        }
         return low;
     }
 };
