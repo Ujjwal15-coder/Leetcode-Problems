b1c648cf5ba16efa7fb0d7f15f02db2ed78abba0
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-01-27 20:08:07 +0530

    Time: 55 ms (26.15%), Space: 53.5 MB (16.31%) - LeetHub

---
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
+        sort(aliceSizes.begin(),aliceSizes.end());
+        sort(bobSizes.begin(),bobSizes.end());
+
+        int sumA = accumulate(aliceSizes.begin(),aliceSizes.end(),0);
+        int sumB = accumulate(bobSizes.begin(),bobSizes.end(),0);
+
+        int avg = (sumA + sumB)/2;
+        int needAlices = avg - sumA;
+
+             unordered_map<int,int> mp;
+             for(int i = 0; i < aliceSizes.size();i++ ){
+                mp[aliceSizes[i]]++;
+             }
+             
+             for(int x:bobSizes){
+                int y = x - needAlices;
+                if(mp.count(y)){
+                    return {y,x};
+                }
+             }
+             
+
+
+        return {};
+
+        
+    }
+};
