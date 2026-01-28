e845db86d490074ac003509e64eb65f564f9517d
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-01-28 21:51:12 +0530

    Time: 0 ms (100%), Space: 20.7 MB (57.6%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xor1 = 0;
+        for(int i:nums){
+            xor1 = xor1 ^ i;
+            
+        }
+        return xor1;
+    }
+};
