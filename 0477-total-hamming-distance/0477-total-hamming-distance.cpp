a193cb7d6a4a3170d1bfecc44f351ccdf5f5ec9e
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-02-02 14:32:19 +0530

    Time: 17 ms (33.24%), Space: 22.9 MB (48.3%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int n = nums.size();
+        int totalDistance = 0;
+
+        for(int i = 0;i < 32;i++)
+        {
+             int count_ones = 0;
+        int count_zeroes = 0;
+            for(int j = 0; j < n;j++){
+                if(((nums[j] >> i) & 1) == 1){
+                    count_ones += 1;
+                }
+                else{
+                    count_zeroes += 1;
+                }
+            }
+            totalDistance += (count_ones * count_zeroes); 
+        }
+        return totalDistance; 
+    }
+};
