fa41d885351ea61c625c5d07519bf5a5408658fb
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-02-04 09:04:09 +0530

    Time: 1 ms (43.42%), Space: 11.1 MB (10.19%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int countTriplets(vector<int>& arr) {
 
-        vector<int> prefix_xor(arr.begin(),arr.end());
+        vector<int> prefix_xor(arr.begin(),arr.end()); //prefix_xor array stores prefix xor of arr
 
-        prefix_xor.insert(prefix_xor.begin(),0);
+        prefix_xor.insert(prefix_xor.begin(),0); //inserting first prefix xor 0
 
         int n = prefix_xor.size();
 
         for(int i = 1; i < n; i++){
-            prefix_xor[i] ^= prefix_xor[i-1];
+            prefix_xor[i] ^= prefix_xor[i-1]; //calculating prefix xor of arr
         }
 
         int count_triplets = 0;
         for(int i = 0; i < n; i++){
             for(int k = i + 1;k < n;k++){
-                if(prefix_xor[k] == prefix_xor[i]){
-                    count_triplets += (k - i - 1);
+                if(prefix_xor[k] == prefix_xor[i]){ //check
+                    count_triplets += (k - i - 1); //as first element stores as 0 so we -1
                 }
             }
         }
