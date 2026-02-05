9fc0915052e9a70117ee0a746d90ba80a9ac4214
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-02-05 19:52:53 +0530

    Time: 0 ms (100%), Space: 25.8 MB (39.55%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n,0);
+        for(int i = 0; i < n; i++){
+            int shift = nums[i]%n;
+            int newIndex = (i + nums[i]) % n;
+
+            if(newIndex < 0){
+                newIndex += n;
+            }
+            result[i] = nums[newIndex];
+        }
+        return result;
+    }
+};
