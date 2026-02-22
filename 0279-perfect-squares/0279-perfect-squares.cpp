fcc7e5e549518eac9101bb9de6906b5717d1620a
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-02-22 20:56:24 +0530

    Time: 51 ms (56.79%), Space: 13.1 MB (68.65%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    int solveMem(int n,vector<int> &dp){
-        if(n == 0)
-        return 0;
+    int solveTab(int n){
+        vector<int> dp(n + 1,INT_MAX);
+       dp[0] = 0;
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
 
         int ans = n;
+        for(int j = 1; j <= n;j++){
         for(int i = 1;i*i <= n; i++){
-            ans = min(ans, 1 + solveMem(n-(i*i),dp));
-        
+            int temp = i * i;
+            if(j - temp >= 0)
+            dp[j] = min(dp[j], 1 + dp[j-temp]);
         }
-        dp[n] = ans;
+        }
         return dp[n];
     }
     int numSquares(int n) {
-        vector<int> dp(n + 1,-1);
-        return solveMem(n,dp);
+      
+        return solveTab(n);
     }
 };
