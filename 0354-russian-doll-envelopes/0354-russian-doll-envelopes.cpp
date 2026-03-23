719d4a266c3370cd6b86b59b169ab5f25b098807
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-03-23 18:28:24 +0530

    Time: 39 ms (71.03%), Space: 81.6 MB (74.42%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        
+        // Step 1: Sort
+        sort(envelopes.begin(), envelopes.end(), [](vector<int>& a, vector<int>& b) {
+            if (a[0] == b[0])
+                return a[1] > b[1];  // height DESC for same width
+            return a[0] < b[0];      // width ASC
+        });
+
+        // Step 2: Apply LIS on heights
+        vector<int> temp;
+
+        for (auto& env : envelopes) {
+            int h = env[1];
+
+            auto it = lower_bound(temp.begin(), temp.end(), h);
+
+            if (it == temp.end())
+                temp.push_back(h);
+            else
+                *it = h;
+        }
+
+        return temp.size();
+    }
+};
