250f3fdeb00e57e422baf27e79a66fe815921747
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-03-30 19:41:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (32.05%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        if(s1[0] == s2[0]){
-            if(s1[2] != s2[2]){
-                return false;
-            }
-        }
-        else{
-            if(!(s1[0] == s2[2] && s1[2] == s2[0])){
-                return false;
-            }
-        }
+       int even[26] ={0};
+        int odd[26] = {0};
 
-        if(s1[1] == s2[1]){
-            if(s1[3] != s2[3]){
-                return false;
+        for(int i = 0; i < 4;i++){
+            if(i % 2 == 0){
+                even[s1[i] - 'a']++;
+                even[s2[i]- 'a']--;
+            }
+            else{
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
             }
         }
-        else{
-             if(!(s1[1] == s2[3] && s1[3] == s2[1])){
+        for(int i = 0; i < 26;i++){
+            if(even[i] != 0 || odd[i] != 0)
                 return false;
-            }
-
         }
         return true;
     }
