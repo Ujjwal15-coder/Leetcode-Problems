85954507314a915dde5e8eb0e5641a291582c7d2
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-04-12 10:39:30 +0530

    Time: 0 ms (100%), Space: 17.9 MB (10.97%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    int minCostClimbingStairs(vector<int>& cost) { //using tabulation method
-    
-    int n = cost.size();
-    int prev2 = cost[0];
-    int prev1 = cost[1];
+    int solveMem(int i,vector<int> &cost,vector<int> &dp){
+        if(i == 0) return cost[0];
+        if(i == 1) return cost[1];
 
-    for (int i = 2; i < n; i++) {
-        int curr = cost[i] + min(prev1, prev2);
-        prev2 = prev1;
-        prev1 = curr;
+        if(dp[i] != -1)
+            return dp[i];
+        
+            int incl = solveMem(i-1, cost, dp);
+            int excl = solveMem(i-2, cost, dp);
+
+            return dp[i] = cost[i] + min(incl,excl);
     }
-
-    return min(prev1, prev2);
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n,-1);
+        return  min(
+            solveMem(n-1, cost, dp),
+            solveMem(n-2, cost, dp));
         
     }
 };
