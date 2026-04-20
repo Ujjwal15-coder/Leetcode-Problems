a247de3bec2b6159cb99a20360c82d15dd03e86b
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-04-20 16:40:38 +0530

    Time: 0 ms (100%), Space: 12.2 MB (6.97%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
-        int n = colors.size();
+        int n= colors.size();
         int result = 0;
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < n;i++){
             if(colors[i] != colors[0])
                 result = max(result,abs(i - 0));
-
-            if(colors[i] != colors[n - 1])
-                result = max(result,abs((n - 1) - i));
+            
+            if(colors[i] != colors[n-1])
+                result = max(result,abs(n-1-i));
         }
         return result;
-        
     }
 };
