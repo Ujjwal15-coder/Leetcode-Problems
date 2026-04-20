1f5b03e69b0e5e6cd8ffe6c38e2724ab52554d25
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-04-20 23:08:24 +0530

    Time: 0 ms (100%), Space: 16.4 MB (14.93%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,7 +1,24 @@
 class Solution {
 public:
+    int solveMem(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& dp){
+        if(i == 0 && j == 0)
+            return grid[0][0];
+        if(i < 0 || j < 0)
+            return 1e9;
+
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int up = grid[i][j] + solveMem(i-1,j,grid,dp);
+        int left = grid[i][j] + solveMem(i,j-1,grid,dp);
+
+        dp[i][j] = min(up,left);
+
+        return dp[i][j];
+
+    }
     int minPathSum(vector<vector<int>>& grid) {
-         int n = grid.size();
+   int n = grid.size();
         int m = grid[0].size();
         //Tabulation
         vector<vector<int>> dp(n,vector<int> (m,0));
