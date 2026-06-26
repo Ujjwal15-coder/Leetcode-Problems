2ebcbe221b76bd97bc326163c4bc33d0cc819c9b
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-06-26 15:54:21 +0530

    Time: 79 ms (54.3%), Space: 114.8 MB (49.01%) - LeetHub

--- 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
+
+        // With comments
+
         int n = nums.size();
+
+        // Cumulative sum till the previous element (before current j)
         int cummSum = 0;
-        unordered_map<int,int> mp;
-        mp[0] = 1; //should be 1 from starting
+
+        // Stores how many times each cumulative sum has appeared
+        unordered_map<int, int> mp;
+
+        // Initially, cumulative sum is 0 before starting
+        mp[0] = 1;
+
+        // Number of valid subarrays ending at current index
         int validPoints = 0;
+
+        // Final answer
         long long ans = 0;
-            for(int j = 0; j< n;j++){
-                if(nums[j] == target){
-                    validPoints += mp[cummSum];
-                    cummSum += 1;
-                }
-                else{
-                    cummSum -= 1;
-                    validPoints -= mp[cummSum];
-                }
-                mp[cummSum] += 1;
-                     ans += validPoints;
+
+        for (int j = 0; j < n; j++) {
+
+            if (nums[j] == target) {
+
+                // Previous cumulative sums equal to current cummSum
+                // become valid after adding the target
+                validPoints += mp[cummSum];
+
+                // Target contributes +1
+                cummSum++;
             }
-                
+            else {
+
+                // Non-target contributes -1
+                cummSum--;
+
+                // Some valid subarrays become invalid
+                validPoints -= mp[cummSum];
+            }
+
+            // Store the current cumulative sum
+            mp[cummSum]++;
+
+            // Add all valid subarrays ending at j
+            ans += validPoints;
+        }
+
         return ans;
     }
 };
