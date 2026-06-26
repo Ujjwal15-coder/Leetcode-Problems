e89706c368f7a06e3ad923db7dfb8aa5463aa715
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-06-26 15:59:46 +0530

    Time: 3 ms (95.67%), Space: 43 MB (18.61%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int cummSum = 0;
         int n = nums.size();
-        int len = 0;
+        int validPoints = 0;
         int ans = 0;
-        for(int i = 0; i < n;i++){
-            int  count = 0;
-            for(int j = i; j < n; j++){
-                if(nums[j] == target)
-                    count++;
-                else
-                    count--;
-            
-            if(count > 0) ans++;
-            }
-            
-        }
 
-        return ans;
+        for(int j = 0; j < n; j++){
+            if(nums[j] == target){
+                validPoints += mp[cummSum];
+                cummSum += 1;
+            }
+            else{
+                cummSum -= 1;
+                validPoints -= mp[cummSum];
+            }
+                mp[cummSum] += 1;
+                ans += validPoints;
+        }
+         return ans;
     }
 };
