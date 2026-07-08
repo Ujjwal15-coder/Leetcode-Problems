0c0b734e17cd9fef1ca1523ef66529334bbf9f60
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-07-08 17:58:26 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int solveMem(int i,int n,vector<int> &dp){
-        if(i == n)
-            return 1;
-        if(i > n)
-            return 0;
-        if(dp[i] != -1)
-            return dp[i];
-        dp[i] = solveMem(i+1,n,dp) + solveMem(i + 2,n,dp);
-        return dp[i];
+    int solveTab(int n){
+        vector<int> dp(n+2);
+        dp[n] = 0;
+        dp[n+1] = 1;
+        for(int i = n-1; i >= 0;i-- ){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0] + dp[1];
     }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solveMem(0,n,dp);
+        return solveTab(n);
     }
 };
