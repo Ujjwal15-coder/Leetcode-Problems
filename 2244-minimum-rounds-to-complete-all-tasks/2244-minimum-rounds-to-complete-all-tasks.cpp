9c8da80c534b30685919d589622995ee4c8fe940
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-07-08 19:32:00 +0530

    Time: 35 ms (33.55%), Space: 107.7 MB (26.34%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int,int> mp;
-
-        for(int &task:tasks){
+        int round = 0;
+        for(int task : tasks){
             mp[task]++;
         }
-        int round = 0;
         for(auto &it:mp){
             int count = it.second;
 
-            if(count == 1)
-                return -1;
-            if(count % 3 == 0) //3*k
-                round += count/3;
+            if(count == 1) return -1;
+            if(count % 3 == 0)
+                round += (count/3);
             else
                 round += (count/3) + 1;
         }
