6329e7b3c02413d44be0963a5e7e5d82eb7fc8d4
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-07-20 19:10:46 +0530

    Time: 2 ms (49.52%), Space: 18.2 MB (66.35%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    void reverse(vector<vector<int>>& grid, int i,int j){
+        int row = grid.size();
+        int col = grid[0].size();
+        while(i < j){
+            swap(grid[i/col][i%col],grid[j/col][j%col]);
+            i++;
+            j--;
+        }
+        
+    }
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int row = grid.size();
+        int col = grid[0].size();
+
+        int n = row * col;
+        k = k % n;
+        if(k == 0) return grid;
+
+        reverse(grid,0,n-1);
+        reverse(grid,0,k-1);
+        reverse(grid,k,n-1);
+
+        return grid;
+        
+    }
+};
