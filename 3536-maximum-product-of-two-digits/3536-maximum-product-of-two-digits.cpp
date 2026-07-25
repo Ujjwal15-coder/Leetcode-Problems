20c60fe4395e80257721935cffa4a5617db2d6f2
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-07-25 07:41:11 +0530

    Time: 0 ms (100%), Space: 9.3 MB (10.11%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -8,12 +8,12 @@ public:
         sort(s.begin(),s.end());
 
         int sL = s.length();
+        // method 1 for fetching last char and second Largest char
+        //  string lastChar(1,s[sL -1]);
+        //  string secondLastChar(1,s[sL - 2]);
 
-         string lastChar(1,s[sL -1]);
-         string secondLastChar(1,s[sL - 2]);
-
-        int num1 = stoi(lastChar);
-        int num2 = stoi(secondLastChar);
+        int num1 = s[sL-1] - '0';
+        int num2 = s[sL-2] - '0';
 
         return num1 * num2;
 
