a01fd713ebd8304cc105e1efac016599cae4babd
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-07-30 19:04:11 +0530

    Time: 0 ms (100%), Space: 8.4 MB (31.81%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int minimumPushes(string word) {
         int ans = 0;
+        int result = 0;
         
         for(int i = 0;i < word.length();i++){
             ans += (i/8) + 1; //() must required
+            result += ans;
         }
         return ans;
     }
