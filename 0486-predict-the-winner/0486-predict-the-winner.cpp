4320a161e46770a6d82c136ab5a3b73fff3cdf8f
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-01 12:06:31 +0530

    Time: 0 ms (100%), Space: 10.1 MB (52.91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int solveRec(int i,int j,vector<int> &nums){
+    int solveMem(int i,int j,vector<int> & nums,vector<vector<int>> &dp){
         if(i > j)
-            return true;
+            return 0;
         if(i == j)
             return nums[i];
-        
-        int take_i = nums[i] + min(solveRec(i+2,j,nums),solveRec(i+1,j-1,nums));
-        int take_j = nums[j] + min(solveRec(i+1,j-1,nums),solveRec(i,j-2,nums));
 
-        return max(take_i,take_j);
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        
+        int take_i = nums[i] + min(solveMem(i+2,j,nums,dp),solveMem(i+1,j-1,nums,dp));
+        int take_j = nums[j] + min(solveMem(i+1,j-1,nums,dp),solveMem(i,j-2,nums,dp));
+
+        return dp[i][j] = max(take_i,take_j);
     }
 
 
     bool predictTheWinner(vector<int>& nums) {
-        int total_Score = accumulate(nums.begin(),nums.end(),0);
         int n = nums.size();
-        int player_1_Score =  solveRec(0,n-1,nums);
+        int total_Score = accumulate(nums.begin(),nums.end(),0);
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int player_1_Score =  solveMem(0,n-1,nums,dp);
         int player_2_Score = total_Score - player_1_Score;
 
         return player_1_Score >= player_2_Score;
