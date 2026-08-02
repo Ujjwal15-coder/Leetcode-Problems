a067cc5dd8056a16469c80b209a8b562e54a8c42
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-02 12:12:51 +0530

    Time: 0 ms (100%), Space: 25.4 MB (65.83%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -9,7 +9,9 @@ public:
         sort(freq.begin(),freq.end(),greater<int>());
 
         int cost = 0;
+
         for(int i = 0; i < 26;i++){
+            
             if(freq[i] == 0)
                 break;
             cost += freq[i] * ((i/8) + 1);
