14f49657588cfaca4695f9e6c00f8567da007319
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-10 17:21:43 +0530

    Time: 0 ms (100%), Space: 12 MB (25.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -6,15 +6,13 @@ public:
         string str1 = strs[0];
         string str2 = strs[n-1];
         int index = 0;
-        while(index < str1.size() && index < str2.size()){
+        while(index < str1.size()  && index < str2.size()){
             if(str1[index] == str2[index]){
                 index++;
-            }
-            else{
+            }else{
                 break;
             }
         }
-            return index==0 ? "" : str1.substr(0,index);
-        
+        return str1.substr(0,index);
     }
 };
