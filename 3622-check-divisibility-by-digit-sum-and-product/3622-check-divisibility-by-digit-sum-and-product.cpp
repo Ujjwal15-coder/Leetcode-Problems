af9e2272f3ff677fe9dce76b7017c45127389fe8
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-23 00:05:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (37.42%) - LeetHub

--- 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
+++ 3622-check-divisibility-by-digit-sum-and-product/3622-check-divisibility-by-digit-sum-and-product.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool checkDivisibility(int n) {
-        int original = n;
+        int original = n; //copying to check further
         int digit;
+
         int sum = 0;
         int prod = 1;
+
         while(n > 0){
-        digit = n % 10;
+        digit = n % 10; //last digit extract karunga
 
         sum += digit;
         prod *= digit;
+
         n = n/10;
         }
         
-        int issekatega = sum + prod;
 
-        if(original % issekatega == 0){
+        if(original % (sum + prod) == 0){
             return true;
             }
         
