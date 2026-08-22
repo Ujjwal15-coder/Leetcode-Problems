bf52d1b2ba38347294f0bcd90236a435f7ad559b
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-22 08:23:17 +0530

    Time: 3 ms (47.14%), Space: 22 MB (47.22%) - LeetHub

--- 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
+++ 0324-wiggle-sort-ii/0324-wiggle-sort-ii.cpp
@@ -1,56 +1,22 @@
-// class Solution {
-// public:
-//     void wiggleSort(vector<int>& nums) {
-//         sort(nums.begin(),nums.end());
-//         int n = nums.size();
-//         int median = nums[n/2];
-
-//         int low = 0;
-//         int mid = 0;
-//         int high = n - 1;
-
-//         while(mid <= high){
-
-//             if(nums[mid] < median){
-//                 swap(nums[low],nums[mid]);
-//                 low++;
-//                 mid++;
-//             }
-//             else if(nums[mid] == median){
-//                 mid++;
-//             }
-//             else{
-//                 swap(nums[mid],nums[high]);
-//                 high--;
-//             }
-
-//         }
-        
-        
-//     }
-// }; --> Wiggle Sort I 
-
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        vector<int> temp = nums;
-
-        sort(temp.begin(),temp.end());
-
         int n = nums.size();
-
+        sort(nums.begin(),nums.end());
+        vector<int> temp = nums;
         int left = (n - 1)/2;
         int right = n - 1;
 
-        for(int i = 0; i < n;i++){
+        for(int i = 0;i < n;i++){
 
             if(i % 2 == 0){
-                nums[i] = temp[left];left--;
+                nums[i] = temp[left];
+                left--;
             }
             else{
-                nums[i] = temp[right];right--;
+                nums[i] = temp[right];
+                right--;
             }
         }
-
     }
 };
