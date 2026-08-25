faad643e6be8a3cd58db0b6de981d15f9540f208
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-25 11:56:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (70.74%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -5,15 +5,15 @@ public:
         int m = matrix[0].size();
 
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < m;j++){
-                swap(matrix[i][j],matrix[j][i]);
+            for(int j = i + 1;j < m;j++){
+                swap(matrix[i][j],matrix[j][i]); //Transpose of a matrix
             }
         }
-        for(int i = 0 ; i < n;i++){
-            for(int j = 0; j < n/2;j++){
-                swap(matrix[i][j],matrix[i][n - 1 - j]);
+        for(int i = 0;i < n; i++){
+            for(int j = 0; j < n/2; j++){
+                swap(matrix[i][j],matrix[i][n-j-1]);
             }
         }
-        return ;
+
     }
 };
