0eb185bc3c7b3de202e7ec0993cc6c82db373de1
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-08-28 10:47:13 +0530

    Time: 0 ms (100%), Space: 33.2 MB (67.7%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -22,6 +22,7 @@ public:
             return root2;
         }
         root1 -> val = root1 -> val + root2 -> val;
+        
         root1 -> left = mergeTrees(root1 -> left,root2 -> left);
         root1 -> right = mergeTrees(root1 -> right,root2 -> right);
 
