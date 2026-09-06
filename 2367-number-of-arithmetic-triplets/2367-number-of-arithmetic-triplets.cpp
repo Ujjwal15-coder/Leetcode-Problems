aa6b8f0000c3151955d9941106c3eeab954d67c4
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-09-07 00:33:24 +0530

    Time: 4 ms (45.81%), Space: 12.1 MB (19.28%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
+        //Optimal Solution -> O(n)
+
         unordered_set<int> st(nums.begin(),nums.end());
 
         int count = 0;
