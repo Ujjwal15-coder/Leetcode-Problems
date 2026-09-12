af264108ecb0ae4589e720bad88071f4fe414347
Ujjwal srivastava <shobhitujjwal99@gmail.com>
2026-09-12 15:20:49 +0530

    Time: 3 ms (9.22%), Space: 10.3 MB (6.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isValid(string s) {
+        stack<int> st;
 
-        stack<char> st;
-        for(int i = 0; i < s.length();i++){
+        for(int i = 0; i < s.size();i++){
 
             char ch = s[i];
 
@@ -11,36 +11,21 @@ public:
                 st.push(ch);
             }
             else{
-
                 if(!st.empty()){
 
                     char top = st.top();
-                    if( (ch ==')' && top == '(') 
-                    
-                    || 
 
-                    (ch =='}' && top == '{') 
-                    
-                    || 
-
-                    (ch ==']' && top == '[')){
+                    if((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')){
                         st.pop();
                     }
                     else{
                         return false;
                     }
-                }
-                else{
-
+                }else{
                     return false;
                 }
             }
         }
-
-            if(st.empty())
-                return true;
-            else
-                return false;
-        
+        return st.empty();
     }
 };
